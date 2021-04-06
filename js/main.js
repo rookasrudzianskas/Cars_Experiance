b4d49fd12ec3eea36a2d480360807ca5eb3a88f6
Rokas <rookas.rudzenskas@gmail.com>
2021-04-06 18:45:33 +0300

    Rokas

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ document.querySelectorAll('.wrapper').forEach((item) => {
     });
   });
 });
+
+const videos = document.querySelectorAll('.video');
+videos.forEach((video) => {
+  video.addEventListener("mouseover", () => {
+    video.play();
+  });
+  video.addEventListener('mouseout', () => {
+    video.pause();
+  });
+});
